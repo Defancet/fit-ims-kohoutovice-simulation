b55369f4e79079ad8d1bf063f9c83c2ea58f8e40
Sereeejja <sergey54324@gmail.com>
2023-12-02 19:04:27 +0100

    feat add new features

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include "simlib.h"
 #include <iostream>
 
-Store lockerRoom("locker Room", 250);
-Store pool("swimming Pool", 230);
+#define WaitUntil(condition) while(_WaitUntil(condition))
+
+Store lockerRoom("locker Room", 200);
+Store pool("swimming Pools", 180);
 Store sauna("sauna", 20);
 
 Facility waterSlide("water Slide");
+int visitors = 0;
 
 class Visitor : public Process {
 public:
     void Behavior() {
+
         Enter(lockerRoom, 1);
         Wait(Exponential(5));
 
@@ -23,10 +27,10 @@ public:
                 swimming:
                 Wait(Exponential(25));
                 double eChoice = Random();
-                if(eChoice <= 0.4){
+                if(eChoice <= 0.7){
                     goto swimming;
                 }
-                else if(eChoice <= 0.6){
+                else if(eChoice <= 0.85){ // change to 0.85
                     goto exitPool;
                 }
                 else{
@@ -37,13 +41,13 @@ public:
             else{
                 toboggan:
                 Seize(waterSlide);
-                Wait(Exponential(2));
+                Wait(Exponential(1));
                 Release(waterSlide);
                 double tChoice = Random();
                 if(tChoice <= 0.7){
                     goto swimming;
                 }
-                else if(tChoice <= 0.75){
+                else if(tChoice <= 0.8){
                     goto exitPool;
                 }
                 else{
@@ -56,10 +60,17 @@ public:
         else{
             // sauna
             saunaLabel:
+
+            double time_start = Time;
+            WaitUntil(!sauna.Full() || Time - time_start > Exponential(7));
+            if(sauna.Full()){
+                goto pool;
+            }
+
             Enter(sauna, 1);
 
             saunaInside:
-            Wait(Exponential(30));
+            Wait(Exponential(20));
 
             double lChoice = Random();
             if(lChoice <= 0.7){
@@ -70,6 +81,7 @@ public:
                 Leave(sauna, 1);
                 Leave(lockerRoom, 1);
                 Wait(Exponential(5)); // prevlek (mb delete)
+                std::cout << "SAUNA EXIT" << std::endl;
                 return;
             }
             else{
@@ -81,9 +93,10 @@ public:
         exitPool:
         Leave(pool, 1);
         double qChoice = Random();
-        if(qChoice <= 0.2){
+        if(qChoice <= 0.3){
             Leave(lockerRoom, 1);
             Wait(Exponential(5)); // prevlel delete ?
+            std::cout << "BASIK EXIT" << std::endl;
             return;
         }
         else{
@@ -97,19 +110,21 @@ class Generator : public Event {
 public:
     void Behavior() {
         (new Visitor)->Activate();
-        Activate(Time+Exponential(5));
+        visitors ++;
+        Activate(Time+Exponential(3));
     }
 
 };
 
 int main()
 {
-    Init(0,100000);
+    Init(0,60*13);
     (new Generator)->Activate();
     Run();
     lockerRoom.Output();
     sauna.Output();
     pool.Output();
     waterSlide.Output();
+    std::cout << visitors << std::endl;
 }
 
